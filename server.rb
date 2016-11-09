d39334f585301b3e58b1345521d90f2521ef909b
rubenDominguezPresa <rubendominguezpresa@hotmail.com>
2016-11-09 20:48:03 +0100

    Initial commit sinatra

---
+++ server.rb
@@ -0,0 +1,36 @@
+# server.rb
+require "sinatra"
+require_relative "./lib/calculator"
+
+get '/' do
+  erb(:calculator)
+end
+
+
+get "/add" do
+  erb(:add)
+end
+
+post "/calculate" do
+  calculator=Calculator.new
+  first = params[:first_number].to_f
+  second = params[:second_number].to_f
+  operation =  params[:operation]
+  case operation
+  	when "addition"
+  		result=calculator.add first, second
+  		"The addition of #{first} and #{second} is #{result}"
+  	when "substract"
+  		result=calculator.substract first, second
+  		"The substract of #{first} and #{second} is #{result}"
+  	when "multiply"
+  		result=calculator.multiply first, second
+  		"The multiply of #{first} and #{second} is #{result}"
+  	when "divide"
+  		result=calculator.divide first, second
+  		"The divide of #{first} and #{second} is #{result}"
+  end
+
+  #result = Calculator.new.add first, second
+  
+end
